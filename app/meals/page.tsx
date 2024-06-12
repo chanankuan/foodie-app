f5a3dad7baa0bd63d5aef7d1f908efc7e4d934c3
Anton Chan <chanankuan1@gmail.com>
2024-06-12 11:56:08 +0200

    feat: add meals page component and css

---
+++ app/meals/page.tsx
@@ -0,0 +1,34 @@
+import { Suspense } from 'react';
+import Link from 'next/link';
+
+import styles from './page.module.css';
+import MealsGrid from '@/components/meals/meals-grid';
+import { getMeals } from '@/lib/meals';
+import { Meal } from '@/lib/definitions';
+
+async function Meals() {
+  const meals: Meal[] = await getMeals();
+
+  return <MealsGrid meals={meals} />;
+}
+
+export default function MealsPage() {
+  return (
+    <>
+      <header className={styles.header}>
+        <h1>
+          Delicious meals, created <span className={styles.highlight}>by you</span>
+        </h1>
+        <p>Choose your favorite recipe and cook it yourself. It is easy and fun!</p>
+        <p className={styles.cta}>
+          <Link href="/meals/share">Share Your Favorite Recipe</Link>
+        </p>
+      </header>
+      <main>
+        <Suspense fallback={<p className={styles.loading}>Fetching meals...</p>}>
+          <Meals />
+        </Suspense>
+      </main>
+    </>
+  );
+}
